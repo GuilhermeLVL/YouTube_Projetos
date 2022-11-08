f8bc60abcc281b13a8fe9f9b61ebdd75a560b352
Guilherme Pinheiro <108196495+GuilhermeLVL@users.noreply.github.com>
2022-11-08 19:11:49 -0300

    Timer que informa as horas, minutos e segundos.
    Um projeto simples usando HTML, CSS e JS, nesse projeto aprendi a criar e acionar funções usando JS.

---
+++ 04-Timer/jss.js
@@ -0,0 +1,34 @@
+var sec=00
+var min = 00
+var hora=00
+
+var intervalo
+
+function iniciar(){
+   intervalo= setInterval(contador,1)
+}
+
+function pausar(){
+    clearInterval(intervalo)
+}
+
+function parar(){
+    clearInterval(intervalo)
+    sec=0
+    min=0 
+    document.getElementById('contador').innerText = min+":"+sec
+}
+
+function contador(){
+    sec++
+    if(sec==60){
+        min++
+        sec=0 
+}
+if(min==60){
+    hora++
+    min=0
+}
+document.getElementById('contador').innerText = hora+":"+min+":"+sec
+
+}
